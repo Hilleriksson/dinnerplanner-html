9321e6086d90414d254f53d65a292bac22cded36
Eduardo Criado <ecriado95@gmail.com>
2017-03-28 15:01:53 +0200

    Layout of scores almost done. Functionality is needed for changing between the Historic and Highest scores.

--- app/js/ScoresController.js
+++ app/js/ScoresController.js
@@ -1,2 +1,16 @@
-/*spotiQuizApp.controller('ScoresController', function ($scope, quizService) {
-});*/
+spotiQuizApp.controller('ScoresController', function($scope, quizService) {
+
+    var userScores = [
+        ["Axel", "90's music", "500", "March 28th 15:21"],
+        ["Arham", "Hip Hop", "500", "March 28th 15:12"],
+        ["Edu", "Indie", "499", "March 28th 14:57"]
+    ];
+
+    $scope.showHistoricScores = function() {
+
+    };
+
+    $scope.showBestScores = function() {
+
+    };
+});
