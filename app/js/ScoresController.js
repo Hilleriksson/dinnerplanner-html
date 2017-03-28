f5663320cc4812fd7dece3a3535e06b4aab03ec0
Eduardo Criado <ecriado95@gmail.com>
2017-03-28 17:08:31 +0200

    Change between different scoreboards is working.

--- app/js/ScoresController.js
+++ app/js/ScoresController.js
@@ -1,15 +1,19 @@
 spotiQuizApp.controller('ScoresController', function($scope) {
 
-            var userScores = [
-                ["Axel", "90's music", "500", "March 28th 15:21"],
-                ["Arham", "Hip Hop", "500", "March 28th 15:12"],
-                ["Edu", "Indie", "499", "March 28th 14:57"]
-            ];
+    var userScores = [
+        ["Axel", "90's music", "500", "March 28th 15:21"],
+        ["Arham", "Hip Hop", "500", "March 28th 15:12"],
+        ["Edu", "Indie", "499", "March 28th 14:57"]
+    ];
 
-            $scope.showHistoricScores = function() {
-                selectType.classList.add("hide");
-            };
+    $scope.moduleState = "historic";
 
-            $scope.showBestScores = function() {
 
-            };
+    $scope.showHistoricScores = function() {
+        $scope.moduleState = "historic";
+    };
+
+    $scope.showBestScores = function() {
+        $scope.moduleState = "highest";
+    };
+});
