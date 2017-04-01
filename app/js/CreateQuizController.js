26b8be6b0f4ddb51d9316adf5de6b74ed4fa5903
amir <amir.moemen@gmail.com>
2017-04-01 22:16:47 +0200

    added artist

--- app/js/CreateQuizController.js
+++ app/js/CreateQuizController.js
@@ -1,22 +1,36 @@
 spotiQuizApp.controller('CreateQuizController', ['$scope', 'Spotify', "$http", function ($scope, Spotify, $http) {
 
-  $scope.songURL = "";
 
+  // Tries to match selected song with URL
+  // Maybe not optimal solution
+  $scope.songURL = "";
   $scope.getURL = function () {
+    var matchCounter = 0;
     if ($scope.searchedSong != ""){
-      Spotify.search($scope.searchedSong, 'track').then(function (data) {
-        console.log(data);
-        $scope.songURL = data.data.tracks.items[0].preview_url;
+      var justSong = $scope.searchedSong.split(", ");
+      Spotify.search(justSong[0], 'track').then(function (data) {
+        while (true) {
+          //console.log(data.data.tracks.items[matchCounter].name + " med "+ justSong[0])
+          //console.log(data.data.tracks.items[matchCounter].artists[0].name  + " med " + justSong[1])
+          if (data.data.tracks.items[matchCounter].name == justSong[0] && data.data.tracks.items[matchCounter].artists[0].name == justSong[1]){
+              $scope.songURL = data.data.tracks.items[matchCounter].preview_url;
+              break;
+          } else {
+            matchCounter++;
+          }
+        }
       });
     };
   };
 
-   $scope.songURL = "";
-   $scope.searchSong = function(val) {
+
+  // Called upon when the typeaheader is called.
+  $scope.searchSong = function(val) {
   return $http.get('//api.spotify.com/v1/search?q='+val+'&type=track', {
   }).then(function(response){
     return response.data.tracks.items.map(function(item){
-      return item.name;
+      var songAndArtist = item.name + ", " + item.artists[0].name;
+      return songAndArtist;
     });
   });
 };
