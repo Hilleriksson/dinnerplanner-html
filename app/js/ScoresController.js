bd5b4e1ca3a5a0f4b0105ee335ea00f51e953753
Eduardo Criado <ecriado95@gmail.com>
2017-04-04 11:01:32 +0200

    Private function for getting scores from firebase.

--- app/js/ScoresController.js
+++ app/js/ScoresController.js
@@ -1,14 +1,5 @@
 spotiQuizApp.controller('ScoresController', function($scope) {
 
-    var userScores = [
-        ["Axel", "90's music", "500", "March 28th 15:21"],
-        ["Arham", "Hip Hop", "500", "March 28th 15:12"],
-        ["Edu", "Indie", "499", "March 28th 14:57"]
-    ];
-
-    $scope.moduleState = "historic";
-
-
     $scope.showHistoricScores = function() {
         $scope.moduleState = "historic";
     };
@@ -16,4 +7,9 @@ spotiQuizApp.controller('ScoresController', function($scope) {
     $scope.showBestScores = function() {
         $scope.moduleState = "highest";
     };
+
+    function _getScores() {
+        // get scores from firebase
+        
+    }
 });
