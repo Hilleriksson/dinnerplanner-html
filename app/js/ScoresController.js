80d178f51d36bce3661e6bc5abc26b7c253affc2
Eduardo Criado <ecriado95@gmail.com>
2017-04-05 18:41:56 +0200

    Test for getting users from DB.

--- app/js/ScoresController.js
+++ app/js/ScoresController.js
@@ -1,18 +1,27 @@
 spotiQuizApp.controller('ScoresController', function($scope, $firebaseObject) {
 
-    // Ref to interact with Firebase
-    var ref = firebase.database().ref();
+  // Ref to interact with Firebase
+  var ref = firebase.database().ref();
 
-    $scope.showHistoricScores = function() {
-        $scope.moduleState = "historic";
-    };
+  $scope.showHistoricScores = function() {
+    $scope.moduleState = "historic";
+  };
 
-    $scope.showBestScores = function() {
-        $scope.moduleState = "highest";
-    };
+  $scope.showBestScores = function() {
+    $scope.moduleState = "highest";
+  };
 
-    function _getScores() {
-        // get scores from firebase
-        
-    }
+  function _getScores() {
+    // get scores from firebase
+    var users = $firebaseObject(ref.child("users"));
+    users.$loaded().then(function() {
+      console.log("Fetched users data from database.");
+      console.log(users.$value);
+    });
+  }
+  $scope.getUsers = function() {
+    _getScores();
+  };
+
+
 });
