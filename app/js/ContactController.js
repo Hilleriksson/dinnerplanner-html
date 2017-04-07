ff15d50eeaae9225cdeebed059ebfa9ab7057b5c
Eduardo Criado <ecriado95@gmail.com>
2017-04-07 13:14:18 +0200

    Modifying contact page and controller to include sidebar and save messages to the db respectively.

--- app/js/ContactController.js
+++ app/js/ContactController.js
@@ -1,4 +1,8 @@
-spotiQuizApp.controller('ContactController', function($scope, $firebaseArray) {
-
-
+spotiQuizApp.controller('ContactController', function($scope, $firebaseObject) {
+    // Save contact message in the db
+    var dbRef = firebase.database().ref();
+    
+    $scope.saveContactMsg = function(firstName, lastName, email, phone, msg) {
+        
+    };
 });
