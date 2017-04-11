c116511dfd97202bcabd0339919f06dd08fefc53
Eduardo Criado <ecriado95@gmail.com>
2017-04-11 15:48:48 +0200

    Form is working now, the controller wasn't added as a js script in the index.html file. AngularFire doesn't close the connection when calling the db, the page gets laggy.

--- app/js/ContactController.js
+++ app/js/ContactController.js
@@ -1,6 +1,6 @@
-spotiQuizApp.controller('ContactController', function($scope, $firebaseObject) {
+spotiQuizApp.controller('ContactController', function($scope, $firebaseArray) {
   // Save contact message in the db
-  var messagesRef = firebase.database().ref().child("contact_msgs").;
+  var messagesRef = firebase.database().ref().child("contact_msgs");
 
   $scope.saveContactMsg = function(firstName, lastName, email, phone, msg) {
     console.log("saveContactMsg() called.");
@@ -11,7 +11,17 @@ spotiQuizApp.controller('ContactController', function($scope, $firebaseObject) {
       name: firstName + " " + lastName,
       phone: phone
     }).then(function(ref) {
-        console.log("Saved contact message to database. Id: " + ref.key);
+      console.log("Saved contact message to database. Id: " + ref.key);
+      $scope.formName = "";
+      $scope.formLastName = "";
+      $scope.formEmail = "";
+      $scope.formPhone = "";
+      $scope.formMsg = "";
+      var bsAlert = document.getElementById("bootstrapAlert");
+      bsAlert.classlist.remove("hidden");
+      setTimeout(function() {
+        bsAlert.classlist.add("hidden");
+      }, 2000);
     });
   }
 });
