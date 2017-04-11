5786be7a8675046e778bb7fc2dd13752380b4eea
Eduardo Criado <ecriado95@gmail.com>
2017-04-11 16:39:17 +0200

    Saving contact message works finally.

--- app/js/ContactController.js
+++ app/js/ContactController.js
@@ -1,27 +1,36 @@
-spotiQuizApp.controller('ContactController', function($scope, $firebaseArray) {
-  // Save contact message in the db
-  var messagesRef = firebase.database().ref().child("contact_msgs");
+spotiQuizApp.controller('ContactController', function($scope) {
 
+  // Save contact message in the db
   $scope.saveContactMsg = function(firstName, lastName, email, phone, msg) {
     console.log("saveContactMsg() called.");
-    var contactMsgs = $firebaseArray(messagesRef);
-    contactMsgs.$add({
+    var childName = "contact_msgs";
+
+    var msgData = {
       email: email,
       message: msg,
       name: firstName + " " + lastName,
       phone: phone
-    }).then(function(ref) {
-      console.log("Saved contact message to database. Id: " + ref.key);
-      $scope.formName = "";
-      $scope.formLastName = "";
-      $scope.formEmail = "";
-      $scope.formPhone = "";
-      $scope.formMsg = "";
-      var bsAlert = document.getElementById("bootstrapAlert");
-      bsAlert.classlist.remove("hidden");
-      setTimeout(function() {
-        bsAlert.classlist.add("hidden");
-      }, 2000);
+    };
+
+    var newMsgKey = firebase.database().ref().child(childName).push().key;
+    var updates = {};
+    updates["/" + childName + "/" + newMsgKey] = msgData;
+    firebase.database().ref().update(updates).then(function() {
+      console.log("Saved contact message to database.");
     });
+
+    // Empty the content of the form
+    $scope.formName = "";
+    $scope.formLastName = "";
+    $scope.formEmail = "";
+    $scope.formPhone = "";
+    $scope.formMsg = "";
+    // Bootstrap alert indicating the user the message has been sent
+    var bsAlert = document.getElementById("bootstrapAlert");
+    bsAlert.classList.remove("hidden");
+    // Hide after 2 seconds
+    setTimeout(function() {
+      bsAlert.classList.add("hidden");
+    }, 2000);
   }
 });
