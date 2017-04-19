0c226a193723b8adf4960f2291ab9ecccadfd557
Eduardo Criado <ecriado95@gmail.com>
2017-04-19 22:02:56 +0200

    All done.

--- app/js/ScoresController.js
+++ app/js/ScoresController.js
@@ -9,19 +9,6 @@ spotiQuizApp.controller('ScoresController', function($scope, $firebaseArray) {
   // quiz is changed 
   $scope.highScoreShow = "showQuiz";
 
-  // TODO use a string comparison of this kind instead of exact match
-  // Levenshtein Distance algo taken from the Internet
-  function compareStrings(s, t) {
-    if (!s.length) return t.length;
-    if (!t.length) return s.length;
-
-    return Math.min(
-      levenshteinDistance(s.substr(1), t) + 1,
-      levenshteinDistance(t.substr(1), s) + 1,
-      levenshteinDistance(s.substr(1), t.substr(1)) + (s[0] !== t[0] ? 1 : 0)
-    ) + 1;
-  }
-
   // Control animation when loading the table
   $scope.scoreContentStatus = "loading";
   // Initialize query to empty string
@@ -40,7 +27,6 @@ spotiQuizApp.controller('ScoresController', function($scope, $firebaseArray) {
     });
   };
 
-  // TODO Populate the select with the options of the most popular quizzes
   $scope.getMostPopularQuizzes = function() {
     console.log("Most popular quizzes gets called.");
     // Empty the list
@@ -75,22 +61,23 @@ spotiQuizApp.controller('ScoresController', function($scope, $firebaseArray) {
   $scope.showSelectedQuiz = function(quizSelected) {
     // Retrieve highest scores for this
     // Have to get them from all_scores table and calculate them
-    console.log("showSelectedQuiz gets called.");
-    console.log(quizSelected + " has been selected.");
+    // console.log("showSelectedQuiz gets called.");
+    // console.log(quizSelected + " has been selected.");
     var allScoresRef = firebase.database().ref().child("all_scores");
     var allScores = $firebaseArray(allScoresRef);
     $scope.highScoresQuiz = [];
     $scope.scoreContentStatus = "loading";
     allScores.$loaded().then(function() {
       var highScoresQuiz = {};
-
+      var highScoresNames = {};
       angular.forEach(allScores, function(score) {
         // Check if same id for quiz, save into user's id key for dict
-        console.log("Score quizId");
-        console.log(score.QUIZID);
+        // console.log("Score quizId");
+        // console.log(score.QUIZID);
         if (score.QUIZID === quizSelected) {
           console.log("Found score for selected quiz.");
           highScoresQuiz[score.USERID] = (highScoresQuiz[score.USERID] || 0) + score.SCORE;
+          highScoresNames[score.USERID] = score.USERNAME;
         }
       });
       // All scores are saved, have to reorder from bigger to lower and save it to score
@@ -111,11 +98,12 @@ spotiQuizApp.controller('ScoresController', function($scope, $firebaseArray) {
       // highScoresQuiz[i][0] contains USERID
       // highScoresQuiz[i][1] contains score for that quiz
       $scope.highScoresQuiz = tuples.reverse();
+      $scope.highScoresNames = highScoresNames;
       $scope.highScoreShow = "showQuiz";
       $scope.scoreContentStatus = "loaded";
     });
   };
-  // TODO Retrieve quiz names matching the input and show high scores for each of them
+
   $scope.searchQuizName = function(quizSearch) {
     $scope.scoreContentStatus = "loading";
     $scope.searchResults = [];
@@ -145,63 +133,39 @@ spotiQuizApp.controller('ScoresController', function($scope, $firebaseArray) {
   };
 
   $scope.searchUsername = function(usernameQuery) {
-    // Query firebase for scores of an specific user
-    // Fetch users and show the scores of the similar one
-    // var users = $firebaseArray(usersRef);
-    // $scope.scoreContentStatus = "loading";
-    // $scope.usernameQuery = usernameQuery;
-    // users.$loaded().then(function() {
-    //   console.log("Query is: " + $scope.usernameQuery);
-    //   console.log("Fetched users data from database.");
-    //   console.log(users);
-
-    //   // Save scores from users with similar name to query, use some distance algo
-    //   // If the query is empty return everything
-    //   if ($scope.usernameQuery === "") {
-    //     $scope.usersContent = users;
-    //   } else {
-    //     var results = [];
-    //     angular.forEach(users, function(user) {
-    //       if (user.name === $scope.usernameQuery) {
-    //         console.log("Found user with similar name: " + user.name);
-    //         results.push({
-    //           "name": user.name,
-    //           "scores": {
-    //             "quiz": user.scores.quiz,
-    //             "score": user.scores.score
-    //           }
-    //         });
-    //       }
-    //     });
-    //     console.log("Logging results array");
-    //     console.log(results);
-    //     $scope.usersContent = results;
-    //   }
-    //   $scope.scoreContentStatus = "loaded";
-    // });
     $scope.scoreContentStatus = "loading";
     var scoresRef = firebase.database().ref().child("all_scores");
     var scores = $firebaseArray(scoresRef);
     scores.$loaded().then(function() {
-      var usersRef = firebase.database().ref().child("users");
-      var users = $firebaseArray(usersRef);
-      users.$loaded().then(function() {
-        if(usernameQuery === "") {
-          // show every score
-          $scope.histScores = scores;
-          $scope.scoreContentStatus = "loaded";
-        } else {
-          // show scores by username
-
-        }
-      });
+      if (usernameQuery === "") {
+        // show every score
+        $scope.histScores = scores.reverse();
+      } else {
+        // show scores by username
+        scoreResult = [];
+        angular.forEach(scores, function(score) {
+          if (score.USERNAME === usernameQuery) {
+            // <td><a href="#!/user/{{score.USERID}}"></a>{{ score.USERNAME }}</td>
+            // <td>{{ score.NAME }}</td>
+            // <td>{{ score.SCORE }}</td>
+            // <td>{{ score.TIMESTAMP }}</td>
+            scoreResult.push({
+              USERNAME: score.USERNAME,
+              USERID: score.USERID,
+              QUIZID: score.QUIZID,
+              NAME: score.NAME,
+              SCORE: score.SCORE,
+              TIMESTAMP: score.TIMESTAMP
+            });
+          }
+        });
+        $scope.histScores = scoreResult.reverse();
+      }
+      $scope.scoreContentStatus = "loaded";
     });
   };
 
-
-
-  // $scope.getUsers();
-  // TODO refactor name to something that makes more sense
+  // Initialise
   $scope.searchUsername("");
   $scope.getMostPopularQuizzes();
 });
